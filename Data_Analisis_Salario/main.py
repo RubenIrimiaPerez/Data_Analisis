06e6fcd4dd90d972faf33fc461bc6a50186fe47b
RubenIrimiaPerez <rubenbaloncesto10@gmail.com>
2024-02-14 11:16:28 +0100

    .

--- Data_Analisis_Salario/main.py
+++ Data_Analisis_Salario/main.py
@@ -2,32 +2,31 @@ import json
 import pandas as pd
 from datetime import datetime
 
-# Función para convertir el salario de dólares a euros y aplicar el aumento del 10% para empleados menores de 30 años
+# Función  salario
 def subida_salario(emp):
     salario = float(emp['salary'].replace('$', '').replace(',', ''))
     if emp.get('age', 0) < 30:
         salario *= 1.1
     return f"{salario:.2f}€"
 
-# Cargar el JSON desde el archivo
+# Cargar el JSON
 with open('employees.json') as f:
     data = json.load(f)
 
-# Filtrar empleados que no son del proyecto 'GRONK'
+#  empleados no 'GRONK'
 data = [emp for emp in data if emp.get('proyect') != 'GRONK']
 
-# Modificar el salario y agregar el símbolo de euro
+
 for emp in data:
     emp['salary'] = subida_salario(emp)
 
-# Crear un DataFrame con los datos y modificarlos
 df = pd.DataFrame(data)
 
-# Generar el nombre del archivo Excel con el formato deseado
+# Generar Excel
 current_date = datetime.now()
 file_name = f"pagos-empleados-{current_date.month}-{current_date.year}.xlsx"
 
-# Guardar el DataFrame en un archivo Excel
+
 df.to_excel(file_name, index=False)
 
 print(f"Archivo Excel '{file_name}' generado correctamente.")
